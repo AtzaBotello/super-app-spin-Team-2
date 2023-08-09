b35c073f694f60aac08ce99a59a788b823956cbf
Atza Botello <alberto.botello@digitalfemsa.com>
2023-08-09 12:36:38 -0600

    feature/global/FUNC-001 Icons Tab Nav

--- navigation/TabNav.tsx
+++ navigation/TabNav.tsx
@@ -1,17 +1,57 @@
-import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {BenefitsScreen} from 'src/screens/Benefits/BenefitsScreen';
-import {HomeScreen} from 'src/screens/Home/HomeScreen';
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { BenefitsScreen } from '@presentation/benefits/screens'
+import { HomeScreen } from '@presentation/home/screens'
+import IconHome from 'src/components/navigation/IconHomeComponent'
+import IconBenefits from 'src/components/navigation/IconBenefitsComponent'
+import IconWallet from 'src/components/navigation/IconWalletComponent'
+import IconProfile from 'src/components/navigation/IconProfileComponent'
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
 export const TabNav = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Beneficios" component={BenefitsScreen} />
-      <Tab.Screen name="Cartera" component={BenefitsScreen} />
-      <Tab.Screen name="Cuenta" component={BenefitsScreen} />
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <IconHome color={color} size={size} />
+          ),
+          tabBarLabel: 'Home',
+        }}
+      />
+      <Tab.Screen
+        name="Beneficios"
+        component={BenefitsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <IconBenefits color={color} size={size} />
+          ),
+          tabBarLabel: 'Beneficios',
+        }}
+      />
+      <Tab.Screen
+        name="Cartera"
+        component={BenefitsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <IconWallet color={color} size={size} />
+          ),
+          tabBarLabel: 'Cartera',
+        }}
+      />
+      <Tab.Screen
+        name="Cuenta"
+        component={BenefitsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <IconProfile color={color} size={size} />
+          ),
+          tabBarLabel: 'Cuenta',
+        }}
+      />
     </Tab.Navigator>
-  );
-};
+  )
+}
