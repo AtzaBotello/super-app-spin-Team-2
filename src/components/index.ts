cb382e7795d7385cf0a1a4a4e64fe120f21ca71e
PabloGastelum-Femsa <jose.gastelum@digitalfemsa.com>
2023-08-09 11:01:44 -0700

    Component to display information with a label and a value

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as MovementsTypeTabView } from './MovementsTypeTabView'
 export { default as MovementsSectionList } from './MovementsSectionList'
 export { default as MovementItem } from './MovementsSectionList/MovementItem'
+export { default as Divider } from './atoms/Divider'
+export { default as InfoSection } from './InfoSection'
